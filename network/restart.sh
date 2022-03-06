04c5a998a7fc42025ec6ff570a2062e696538c10
artas182x <artas182x@gmail.com>
2022-03-06 19:56:32 +0100

    Prepare 4 organisations for minio and backend scaling

--- network/restart.sh
+++ network/restart.sh
@@ -4,3 +4,4 @@
 cp ./organizations/peerOrganizations/org1.example.com/connection-org1.yaml ../web/network1.yaml && chmod 644 ../web/network1.yaml
 cp ./organizations/peerOrganizations/org2.example.com/connection-org2.yaml ../web/network2.yaml && chmod 644 ../web/network2.yaml
 cp ./organizations/peerOrganizations/org3.example.com/connection-org3.yaml ../web/network3.yaml && chmod 644 ../web/network3.yaml
+cp ./organizations/peerOrganizations/org4.example.com/connection-org4.yaml ../web/network4.yaml && chmod 644 ../web/network4.yaml
